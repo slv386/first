fae4310c106daa39f0901eb459bb1b2d0dcc2828
slv <slv@l1523.ru>
2020-11-20 17:55:52 +0300

    Second comment

--- first.py
+++ first.py
@@ -2,6 +2,9 @@ def main():
     print('My first git program')
     print('And I change it every day')
     print('Again')
+    print('My first git program')
+    print('And I change it every day')
+    print('Again')
 
 if __name__ == '__main__':
     main()
